e9aa2c2a3633952707ea21c0fe3ea596352441bd
Casey <caseycorvino7@gmail.com>
2018-07-06 09:19:22 -0400

    bike

---
+++ Assets/Assignments/Assignment_04/A04_cc5341/Scripts/GenerateBike.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A04_cc5341
+{
+
+    public class GenerateBike : MonoBehaviour
+    {
+        public GameObject[] wheels;
+        public GameObject[] frame;
+       
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
