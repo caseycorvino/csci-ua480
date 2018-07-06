570f0060d9f993f54107f058b961616f3546c328
Casey <caseycorvino7@gmail.com>
2018-07-05 22:57:10 -0400

    two thirds done

---
+++ Assets/Assignments/Assignment_04/A04_cc5341/Scripts/move_player_with_acceleration.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace A04_cc5341
+{
+    public class move_player_with_acceleration : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = transform.GetComponent<Rigidbody>();
+
+
+        }
+
+        bool moving = false;
+        public float movementSpeed = 2f;
+        // Update is called once per frame
+
+        Rigidbody rb;
+        bool decelerate = false;
+        bool accelerate = false;
+        float decelerateAmount = 0f;
+        float accelerateAmount = 0f;
+        float headY = 0f;
+        void Update()
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+
+                //start walking
+                if (accelerate == false && moving == false)//and no ray cast
+                {
+                    accelerate = true;
+                    moving = true;
+                   
+                    //then accelerate
+                }
+                else if (accelerate == true)
+                {
+                   
+                    Vector3 forward = Camera.main.transform.forward;
+                    forward.y = Mathf.Sin(headY + Time.time * 8) / 5;
+                    transform.position += forward * Time.deltaTime * (accelerateAmount / 100);
+
+                    if (accelerateAmount >= 200f)
+                    {
+                        accelerate = false;
+                        accelerateAmount = 0f;
+                    }
+                    accelerateAmount += 1f;
+                    //then move normally 
+                    decelerateAmount += 1f;
+                }
+                else
+                {
+                   
+                    moving = true;
+                    Vector3 forward = Camera.main.transform.forward;
+                    forward.y = Mathf.Sin(headY + Time.time * 8) / 5;
+                    transform.position += forward * Time.deltaTime * movementSpeed;
+                }
+
+            }
+
+            //stop clicking
+            if (moving && !Input.GetMouseButton(0))
+            {
+
+                // rb.AddForce(-(Camera.main.transform.forward) * Time.deltaTime * movementSpeed);
+                moving = false;
+                decelerate = true;
+            }
+
+            //decelerate when done
+            if (decelerate)
+            {
+                
+                Vector3 forward = Camera.main.transform.forward;
+                forward.y = Mathf.Sin(headY + Time.time * 8) / 5;
+                transform.position += forward * Time.deltaTime * (decelerateAmount / 100);
+
+                if (decelerateAmount <= 0f)
+                {
+                    decelerate = false;
+                    decelerateAmount = 0f;
+                }
+                decelerateAmount -= 1f;
+            }
+            //stop accelerating
+            if (!Input.GetMouseButton(0))
+            {
+                accelerate = false;
+                accelerateAmount = 0f;
+            }
+
+
+
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_04/A04_cc5341/Scripts/teleport.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace A04_cc5341
+{
+    [RequireComponent(typeof(Collider))]
+    public class teleport : MonoBehaviour, IPointerClickHandler
+    {
+        
+        private Collider _collider;
+
+        private void Start()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+        {
+            float c = eventData.pointerPressRaycast.distance;//raycast to hit
+            float a = 1; // height of player
+            float b = Mathf.Sqrt(Mathf.Pow(c, 2) - a);//distace forward to hit from floor
+
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0;
+
+            GameObject.Find("Player").transform.position += forward * b;//need to change
+        }
+
+
+        //used your code
+        private void OnTriggerEnter(Collider other)
+        {
+            //Disable the collision detection on this collider when the camera is inside of it, so the casting ray won't be blocked by it.
+            if (other.CompareTag("MainCamera"))
+            {
+                _collider.isTrigger = true;
+            }
+        }
+
+        //used your code
+        private void OnTriggerExit(Collider other)
+        {
+            //Re-enable the collision detection when camera exits the collider
+            if (other.CompareTag("MainCamera"))
+            {
+                _collider.isTrigger = false;
+            }
+        }
+    }
+}
